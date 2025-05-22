4ec56199426cb55cc998fcd3b2ad262512adb19e
Polina Buchina <PolinaBuchina2002@yandex.ru>
2025-05-22 23:01:48 +0300

    Объединение 4

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-print("somthing changr")
+print("somthrng changr")
 print("somthing changr")
 
 def my_little_pain(head):
     print(head)
+    print(99999)
 new_changes = 22 ** 5
 print(new_changes)
